f02e6eebb3003da8dbeb5b82a98bee8197471d09
Scott Bellware <sbellware@gmail.com>
2015-03-06 11:44:47 -0600

    Environment variables for use in testing

--- test/test_init.rb
+++ test/test_init.rb
@@ -1,3 +1,9 @@
+ENV['CONSOLE_DEVICE'] ||= 'stdout'
+ENV['CONSOLE_COLORS'] ||= 'on'
+ENV['LOG_LEVEL'] ||= 'trace'
+ENV['FILE_LOGGING'] ||= 'on'
+ENV['ERROR_FILE_LOGGING'] ||= 'on'
+
 puts RUBY_DESCRIPTION
 
 require_relative '../init.rb'
