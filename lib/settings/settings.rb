549fe575fda22f8cdbf6a9489e8fd49f1c3daaa5
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-03-09 17:16:56 -0500

    Use default settings file pathname if none is passed into the build method.

--- lib/settings/settings.rb
+++ lib/settings/settings.rb
@@ -7,7 +7,10 @@ class Settings
     @data = data
   end
 
-  def self.build(pathname)
+  def self.build(pathname=nil)
+    # Does this need to be tested again? Is it even necessary?
+    pathname ||= File.instance.pathname
+
     file_data = ::File.open(pathname)
 
     data = JSON.load file_data
@@ -21,6 +24,7 @@ class Settings
   end
 
   class File
+    # Is there a use for setting directory/name separately versus together?
     attr_accessor :directory
     attr_accessor :name
 
@@ -45,4 +49,25 @@ class Settings
       end
     end
   end
+
+  # class File
+  #   attr_accessor :directory
+
+  #   def self.instance
+  #     @instance ||= new
+  #   end
+
+  #   def pathname
+  #     directory = Pathname.new self.directory.to_s
+  #     name = Pathname.new Defaults.name
+
+  #     pathname = (directory + name).to_s
+  #   end
+
+  #   module Defaults
+  #     def self.name
+  #       'settings.json'
+  #     end
+  #   end
+  # end
 end
