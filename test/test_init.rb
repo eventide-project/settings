4ad66e1e925edec386733d86480881c036926bd4
Scott Bellware <sbellware@gmail.com>
2015-03-09 00:04:19 -0500

    CONSOLE_COLOR, rather than CONSOLE_COLORS

--- test/test_init.rb
+++ test/test_init.rb
@@ -1,5 +1,5 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
-ENV['CONSOLE_COLORS'] ||= 'on'
+ENV['CONSOLE_COLOR'] ||= 'on'
 ENV['LOG_LEVEL'] ||= 'trace'
 ENV['FILE_LOGGING'] ||= 'on'
 ENV['ERROR_FILE_LOGGING'] ||= 'on'
