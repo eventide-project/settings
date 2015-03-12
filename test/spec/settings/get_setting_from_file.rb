0a294a23f508375a2f1e737a408c910af5f91b7c
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-03-12 13:04:12 -0500

    Remove unnecessary test.

--- test/spec/settings/get_setting_from_file.rb
+++ test/spec/settings/get_setting_from_file.rb
@@ -1,14 +1,5 @@
 describe Settings do
-  specify "Getting a setting from the default filename given a directory path" do
-    settings_directory = File.dirname(File.expand_path(__FILE__))
-    settings = Settings.build(settings_directory)
-    keys = ['some_setting', 'some_nested_setting']
-    setting = settings.get keys
-
-    expect(setting == 'some nested value').to be
-  end
-
-  specify "Getting a setting from a file given a filepath" do
+  specify "Getting a setting from a file" do
     settings_file = File.join(File.dirname(File.expand_path(__FILE__)), "single_setting.json")
     settings = Settings.build(settings_file)
     key = 'some_setting'
