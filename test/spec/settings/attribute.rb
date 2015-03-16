f218d896ffc5e543fe9a17cb8dafcb28d79fc9cf
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-03-16 15:52:17 -0500

    Settings attributes are registered in the settings registry on instantiation.

--- test/spec/settings/attribute.rb
+++ test/spec/settings/attribute.rb
@@ -3,6 +3,7 @@ module AttributeExample
     extend Settings::Setting::Macro
 
     setting :some_setting
+    attr_accessor :some_attribute
   end
 end
 
@@ -15,5 +16,19 @@ describe "Settings Attribute" do
   specify "Has a setter" do
     thing = AttributeExample::Thing.new
     expect(thing.respond_to? :some_setting=).to be
+  end
+
+  specify "Is in the Settings registry" do
+    registry = Settings::Registry.instance
+
+    expect(registry.setting? AttributeExample::Thing, :some_setting).to be
+  end
+end
+
+describe "Attribute Accessor" do
+  specify "Is not in the Settings registry" do
+    registry = Settings::Registry.instance
+
+    expect(registry.setting? AttributeExample::Thing, :some_attribute).to_not be
   end
 end
