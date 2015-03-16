b5dc31d0d07ad124cbe12f4ecb803105ec61eda3
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-03-16 15:38:06 -0500

    Renamed registry.rb file.

--- test/spec/settings/registry.rb
+++ test/spec/settings/registry.rb
@@ -1,20 +1,20 @@
-class SomeObject
-  setting :some_setting
-  setting :some_other_setting
+class ExampleObject
+  attr_accessor :some_setting
+  attr_accessor :some_other_setting
 end
 
 describe Settings::Registry do
   specify "A registered setting will be stored in the settings registry by its class name" do
     registry = Settings::Registry.instance
 
-    registry.register(SomeObject, :some_setting)
+    registry.register(ExampleObject, :some_setting)
 
-    expect(registry.setting? SomeObject, :some_setting ).to be
+    expect(registry.setting? ExampleObject, :some_setting ).to be
   end
 
   specify "An unregistered setting will not be stored in the settings registry" do
     registry = Settings::Registry.instance
 
-    expect(registry.setting? SomeObject, :some_other_setting ).to_not be
+    expect(registry.setting? ExampleObject, :some_other_setting ).to_not be
   end
 end
