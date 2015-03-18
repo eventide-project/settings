346b95239b57272d4aa834277769c23932e5d9f1
Scott Bellware <sbellware@gmail.com>
2015-03-18 16:23:22 -0500

    The voicing of the specs should be closer to imperative than simple present tense

--- test/spec/settings/assignment.rb
+++ test/spec/settings/assignment.rb
@@ -8,21 +8,21 @@ class AnotherObject
 end
 
 describe Settings::Setting::Assignment do
-  specify "Determines whether the attribute is in the settings registry" do
+  specify "Determine whether the attribute is in the settings registry" do
     another_object = AnotherObject.new
 
     expect(Settings::Setting::Assignment.setting? another_object, :some_setting).to be
     expect(Settings::Setting::Assignment.setting? another_object, :some_attribute).to_not be
   end
 
-  specify "Determines whether the attribute has a setter method" do
+  specify "Determine whether the attribute has a setter method" do
     another_object = AnotherObject.new
 
     expect(Settings::Setting::Assignment.assignable? another_object, :some_setting).to be
     expect(Settings::Setting::Assignment.assignable? another_object, :some_attribute).to_not be
   end
 
-  specify "Assigns the given value to the corresponding attribute on the receiver" do
+  specify "Assign to the corresponding attribute on the receiver" do
     another_object = AnotherObject.new
 
     Settings::Setting::Assignment.assign_value(another_object, :some_setting, "some value")
