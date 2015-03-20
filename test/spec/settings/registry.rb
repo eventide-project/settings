df863674e5b22d6643ed63180bc612235d0844f5
Scott Bellware <sbellware@gmail.com>
2015-03-20 08:37:25 -0500

    The registry tests are clarified before making changes to the registry interface

--- test/spec/settings/registry.rb
+++ test/spec/settings/registry.rb
@@ -1,20 +1,15 @@
-class ExampleObject
-  attr_accessor :some_setting
-  attr_accessor :some_other_setting
-end
-
-describe Settings::Registry do
+describe Settings::Registry, :* do
   specify "A registered setting will be stored in the settings registry by its class name" do
     registry = Settings::Registry.instance
 
-    registry.register(ExampleObject, :some_setting)
+    registry.register(Object, :some_setting)
 
-    expect(registry.setting? ExampleObject, :some_setting ).to be
+    expect(registry.setting? Object, :some_setting ).to be true
   end
 
   specify "An unregistered setting will not be stored in the settings registry" do
     registry = Settings::Registry.instance
 
-    expect(registry.setting? ExampleObject, :some_other_setting ).to_not be
+    expect(registry.setting? Object, :some_other_setting ).to_not be true
   end
 end
