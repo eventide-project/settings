2d13eb0802f3fa6922b4432768a1df712008c5fb
Scott Bellware <sbellware@gmail.com>
2015-03-22 01:26:54 -0500

    Test uses control module

--- test/spec/settings/get_setting_from_file.rb
+++ test/spec/settings/get_setting_from_file.rb
@@ -1,10 +1,10 @@
-describe Settings do
+describe "Getting a setting from a file" do
   specify "Getting a setting from a file" do
     settings_file = File.join(File.dirname(File.expand_path(__FILE__)), "single_setting.json")
     settings = Settings.build(settings_file)
-    key = 'some_setting'
-    setting = settings.get key
 
-    expect(setting == 'some value').to be
+    setting = settings.get 'some_setting'
+
+    assert(setting == 'some value')
   end
 end
