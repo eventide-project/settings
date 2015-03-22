c2c27adc7513a11dd9ee45c581a79a9b2aa84521
Scott Bellware <sbellware@gmail.com>
2015-03-22 01:21:39 -0500

    Test uses a control module

--- test/spec/settings/get_setting.rb
+++ test/spec/settings/get_setting.rb
@@ -1,21 +1,27 @@
-data = {
-  "some_setting" => "some value"
-}
+module GetSetting
+  def self.data
+    {
+      "some_setting" => "some value"
+    }
+  end
 
-settings = Settings.new data
+  def self.settings
+    Settings.new data
+  end
 
-describe Settings do
+  def self.get(key)
+    settings.get key
+  end
+end
+
+describe "Get Settings", :* do
   specify "Getting a setting from the setting data gets the setting's value" do
-    key = 'some_setting'
-    setting = settings.get key
-
+    setting = GetSetting.get 'some_setting'
     expect(setting == 'some value').to be
   end
 
   specify "Trying to get a setting that isn't in the data gets a nil" do
-    key = 'setting_that_isnt_in_the_data'
-    setting = settings.get key
-
+    setting = GetSetting.get 'setting_that_isnt_in_the_data'
     expect(setting == nil).to be
   end
 end
