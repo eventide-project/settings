cf22cb9b733efcf67f914b621a2676e1ae4ce44b
Scott Bellware <sbellware@gmail.com>
2015-04-15 14:21:29 -0500

    prox_gem is updated with the latest version

--- prox_gem.rb
+++ prox_gem.rb
@@ -22,7 +22,11 @@ def _prox_gem_mode
   "off"
 end
 
-def _gem_dir
+def _print_prox_gem_mode
+  puts "PROX_GEM_MODE: #{_prox_gem_mode}"
+end
+
+def _prox_gem_dir
   gem_dir = ENV['PROX_GEM_DIR']
 
   return gem_dir if _set? gem_dir
@@ -31,13 +35,21 @@ def _gem_dir
   raise
 end
 
+def _print_prox_gem_dir
+  puts "PROX_GEM_DIR: #{ENV['PROX_GEM_DIR']}"
+end
+
 def _prox_gem_git_uri
-  get_uri = ENV['PROX_GEM_GIT_URI']
+  git_uri = ENV['PROX_GEM_GIT_URI']
 
   return git_uri if _set? git_uri
 
   _print_git_repo_uri_error
   raise
+end
+
+def _print_prox_gem_git_uri
+  puts "PROX_GEM_GIT_URI: #{ENV['PROX_GEM_GIT_URI']}"
 end
 
 def _set?(val)
@@ -46,7 +58,7 @@ end
 
 def _file_system_path(gem_name)
   gem_name = _canonical_gem_name(gem_name)
-  File.join(_gem_dir, gem_name)
+  File.join(_prox_gem_dir, gem_name)
 end
 
 def _git_path(gem_name, options)
@@ -74,3 +86,7 @@ end
 def _print_git_repo_uri_error
   puts 'The PROX_GEM_GIT_URI environment variable must be set to the URI of the Git repository where the gem project is housed'
 end
+
+_print_prox_gem_mode
+_print_prox_gem_dir
+_print_prox_gem_git_uri
