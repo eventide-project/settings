5a037821225a3a0425701ebc5457236741cffe5a
Scott Bellware <sbellware@gmail.com>
2015-04-21 02:37:18 -0500

    Specifications for profile (previously, "topic") overrides

---
+++ test/spec/data_source/profile.rb
@@ -0,0 +1,6 @@
+describe "Data Source Profile" do
+  specify "activated by the PROFILE environment variable"
+  specify "loads the base settings from the base (predecessor) directory"
+  specify "adds the profile to the file path"
+  specify "overrides the base settings with the profile settings"
+end
