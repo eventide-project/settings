7c4ec8b40e4a223c60fc35187b3808fe02d4063d
Scott Bellware <sbellware@gmail.com>
2015-04-21 02:39:37 -0500

    Specifications for the profile base behavior

--- test/spec/data_source/profile.rb
+++ test/spec/data_source/profile.rb
@@ -3,4 +3,6 @@ describe "Data Source Profile" do
   specify "loads the base settings from the base (predecessor) directory"
   specify "adds the profile to the file path"
   specify "overrides the base settings with the profile settings"
+  specify "the profile base directory is the predecessor directory"
+  specify "the profile base directory can be overridden with the PROFILE_BASE environment variable"
 end
