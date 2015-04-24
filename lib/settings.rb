414016f6cbfcc32ceb5a6b1388293150bbfc4376
Scott Bellware <sbellware@gmail.com>
2015-04-24 01:07:50 -0500

    Vestigial file is removed

--- lib/settings.rb
+++ lib/settings.rb
@@ -9,9 +9,6 @@ require 'settings/data_source/hash'
 require 'settings/data_source/file'
 require 'settings/data_source/data_source'
 require 'settings/registry'
-
-require 'settings/file'
-
 require 'settings/settings'
 require 'settings/activate'
 require 'settings/setting/assignment'
