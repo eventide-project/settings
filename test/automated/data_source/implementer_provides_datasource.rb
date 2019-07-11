397705d3e85359ce493e621e5fef1848b782169b
Scott Bellware <sbellware@gmail.com>
2019-07-11 11:41:09 -0500

    Test description is corrected

--- test/automated/data_source/implementer_provides_datasource.rb
+++ test/automated/data_source/implementer_provides_datasource.rb
@@ -4,7 +4,7 @@ context "Data Source" do
   context "Implementer Provides the Data Source" do
     settings = Controls::DataSource::Implementer.example
 
-    test "A subclass can specify the datasource by implementing a class method named 'datasource'" do
+    test "A subclass can specify the data_source by implementing a class method named 'data_source'" do
       assert(settings.data == Controls::DataSource::Hash.data)
     end
   end
